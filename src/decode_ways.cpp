3070ddc261996f244c416b482b609eb9c8ab2461
root <account-login@users.noreply.github.com>
2016-08-29 19:14:01 +0800

    [decode_ways] fix check; passed

--- src/decode_ways.cpp
+++ src/decode_ways.cpp
@@ -19,15 +19,27 @@ using namespace std;
 class Solution {
 public:
     int numDecodings(const string &s) {
-        if (s.size() <= 1) {
-            return 1;
+        if (s.size() == 0) {
+            return 0;
         }
 
         int ans[s.size() + 1];
         ans[0] = 1;
-        ans[1] = 1;
+        if ('1' <= s[0] && s[0] <= '9') {
+            ans[1] = 1;
+        } else {
+            return 0;
+        }
+
         for (size_t i = 2; i <= s.size(); i++) {
-            ans[i] = ans[i - 1];
+            ans[i] = 0;
+
+            if ('1' <= s[i - 1] && s[i - 1] <= '9') {
+                ans[i] += ans[i - 1];
+            } else if (s[i - 1] != '0') {
+                return 0;
+            }
+
             if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6')) {
                 ans[i] += ans[i - 2];
             }
@@ -42,7 +54,12 @@ public:
 TEST_CASE("91. Decode Ways") {
     Solution s;
 
-    CHECK(s.numDecodings("") == 1);
+    CHECK(s.numDecodings("0") == 0);
+    CHECK(s.numDecodings("") == 0);
+    CHECK(s.numDecodings("01") == 0);
+
+    CHECK(s.numDecodings("10") == 1);
+
     CHECK(s.numDecodings("1") == 1);
     CHECK(s.numDecodings("12") == 2);
     CHECK(s.numDecodings("121") == 3);
