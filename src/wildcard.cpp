d99cfdad759a4db67f9541cda9ecc23bf89a087e
root <account-login@users.noreply.github.com>
2016-08-31 00:07:49 +0800

    [wildcard] add some docs.

--- src/wildcard.cpp
+++ src/wildcard.cpp
@@ -17,7 +17,7 @@ using namespace std;
 
 class Solution {
 public:
-    bool isMatch(string s, string p) {
+    bool isMatch(const string &s, const string &p) {
         const char *str = s.data();
         const char *pattern = p.data();
         const char *pattern_begin = pattern;
@@ -64,7 +64,7 @@ public:
                 }
             } else {
                 // match middle word
-                jmp = this->find_word(str, pattern);
+                jmp = this->find_word_kmp(str, pattern);
                 if (jmp < 0) {
                     return false;
                 } else {
@@ -78,6 +78,16 @@ public:
         }
     }
 
+    /**
+     * @brief Find out weither str starts with first word of \a pattern.
+     *
+     *        \a pattern is a sequence of word seperated by '*'.
+     *        A word may contain '?'.
+     * @param str
+     * @param pattern
+     * @return return the length of first word of pattern if str starts with
+     *         first word of \a pattern, otherwise return a negative number.
+     */
     int cmp_word(const char *str, const char *pattern) {
         for (size_t i = 0; ; i++) {
             if (pattern[i] == '\0' || pattern[i] == '*') {
@@ -93,7 +103,13 @@ public:
         }
     }
 
-    // naive
+    /**
+     * @brief search the first word of \a pattern in \a str. A naive method.
+     * @param str
+     * @param pattern
+     * @return return the offset from \a str to the end of matched word on success,
+     *         return a negative number on fail.
+     */
     int find_word_naive(const char *str, const char *pattern) {
         const char *str_begin = str;
         do {
@@ -108,7 +124,14 @@ public:
         return -1;
     }
 
-    bool memcmp_question_mark(const char *buf1, const char *buf2, size_t size) {
+    /**
+     * @brief memcmp() that ignore '?' on both buffer. Used by KMP algorithm.
+     * @param buf1
+     * @param buf2
+     * @param size
+     * @return return weither \a buf1 equals \a buf2.
+     */
+    bool memcmp_qm(const char *buf1, const char *buf2, size_t size) {
         for (size_t i = 0; i < size; i++) {
             if (buf1[i] == '?' || buf2[i] == '?') {
                 continue;
@@ -121,6 +144,14 @@ public:
         return true;
     }
 
+    /**
+     * @brief Find out the longest prefix of \a str that matches the prefix of \a pattern.
+     * @param str
+     * @param pattern
+     * @param start   skip first n characters of \a str. Used by KMP algorithm.
+     * @return return the length of matched prefix.
+     *         return a negative number on fail.
+     */
     int match_partial(const char *str, const char *pattern, int start = 0) {
         for (int ret = start; ; ret++) {
             if (pattern[ret] == '*' || pattern[ret] == '\0') {
@@ -139,8 +170,14 @@ public:
         }
     }
 
-    // KMP
-    int find_word(const char *str, const char *pattern) {
+    /**
+     * @brief search the first word of \a pattern in \a str using KMP algorithm.
+     * @param str
+     * @param pattern
+     * @return return the offset from \a str to the end of matched word on success,
+     *         return a negative number on fail.
+     */
+    int find_word_kmp(const char *str, const char *pattern) {
         int pattern_len = 0;
         while (pattern[pattern_len] != '*' && pattern[pattern_len] != '\0') {
             pattern_len++;
@@ -152,7 +189,7 @@ public:
             jmp_table[i] = 1;
             for (int j = 1; j <= i; j++) {
                 int size = i - j;
-                if (memcmp_question_mark(pattern + j, pattern, size)) {
+                if (memcmp_qm(pattern + j, pattern, size)) {
                     jmp_table[i] = j;
                     break;
                 }
@@ -167,13 +204,16 @@ public:
                 return -1;
             }
 
+            // the whole word matched, success.
             if (prefix_len == pattern_len) {
                 return str - origin_str + pattern_len;
             }
 
+            // go to the next posible matched position
             int jmp = jmp_table[prefix_len];
             str += jmp;
 
+            // skip matched prefix
             if (prefix_len > 0) {
                 start = prefix_len - jmp;
             } else {
