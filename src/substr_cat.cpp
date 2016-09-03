3e43e591094af55224d645eb7cba4b3dfcf4c9f3
root <account-login@users.noreply.github.com>
2016-09-03 20:00:13 +0800

    [substr_cat] use vector instead of hash table, boosted.

--- src/substr_cat.cpp
+++ src/substr_cat.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <algorithm>
 #include <unordered_set>
+#include <unordered_map>
 
 #ifdef RUN_TEST
 #   define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
@@ -26,17 +28,106 @@ public:
         int word_len = words[0].size();
         int block_len = word_len * words.size();
 
+        unordered_map<string, int> word2id;
+        for (size_t i = 0; i < words.size(); i++) {
+            if (word2id.count(words[i]) == 0) {
+                word2id[words[i]] = i;
+            }
+        }
+        int uniq_word_count = word2id.size();
+
+        vector<int> word_count(uniq_word_count, 0);
+        for (auto &word : words) {
+            word_count[word2id[word]]++;
+        }
+
+        vector<int> ans;
+
+        // O( len(word) * (len(s) / len(word)) * len(word) )
+        //    ^ start     ^ word_idx             ^ hash
+        // = O( len(word) * len(s) )
+        for (int start = 0; start < word_len && start <= (int)s.size() - block_len; start++) {
+            vector<int> visited(uniq_word_count, 0);
+            size_t queue_size = 0;
+            size_t word_idx = 0;
+            vector<bool> ok_word(uniq_word_count, false);
+
+            while (start + (word_idx + 1) * word_len <= s.size()) {
+                int word_pos = start + word_idx * word_len;
+                string word = s.substr(word_pos, word_len);
+
+                auto found = word2id.find(word);
+                if (found == word2id.end()) {
+                    // not matched, clear queue & set
+                    queue_size = 0;
+                    fill(visited.begin(), visited.end(), 0);
+                    fill(ok_word.begin(), ok_word.end(), false);
+                } else {
+                    // new matched word
+                    int id = found->second;
+                    queue_size++;
+                    visited[id]++;
+
+                    // visited counter modified, adjust ok_word set
+                    if (visited[id] == word_count[id]) {
+                        ok_word[id] = true;
+                    } else {
+                        ok_word[id] = false;
+                    }
+
+                    // queue too long, drop front
+                    if (queue_size > words.size()) {
+                        assert(queue_size == words.size() + 1);
+                        int index = word_pos - (queue_size - 1) * word_len;
+                        string queue_front = s.substr(index, word_len);
+                        int front_id = word2id[queue_front];
+
+                        // decrease found word count
+                        visited[front_id]--;
+
+                        // visited counter modified, adjust ok_word set
+                        if (visited[front_id] == word_count[front_id]) {
+                            ok_word[front_id] = true;
+                        } else {
+                            ok_word[front_id] = false;
+                        }
+
+                        queue_size--;
+                        assert(queue_size == words.size());
+                    }
+
+                    // anwser found
+                    if (queue_size == words.size()
+                        && count(ok_word.begin(), ok_word.end(), true) == uniq_word_count)
+                    {
+                        int index = word_pos - (queue_size - 1) * word_len;
+                        ans.push_back(index);
+                    }
+                }
+
+                word_idx++;
+            }
+        }
+
+        return ans;
+    }
+
+    vector<int> findSubstring_naive(const string &s, const vector<string> &words) {
+        if (words.size() == 0) {
+            return {};
+        }
+
+        int word_len = words[0].size();
+        int block_len = word_len * words.size();
+
         unordered_multiset<string> word_set;
         unordered_set<string> uniq_word_set;
         for (auto &word : words) {
             word_set.insert(word);
             uniq_word_set.insert(word);
         }
-        size_t uniq_word_count = uniq_word_set.size();
 
         vector<int> ans;
-
-        /* naive search
         // O( len(s) * len(words) * len(word) )
         //    ^ i      ^ word_idx   ^ hash
         for (int i = 0; i <= (int)s.size() - block_len; i++) {
@@ -63,67 +154,6 @@ public:
 
             if (ok) {
                 ans.push_back(i);
-            }
-        }
-        */
-
-        // O( len(word) * (len(s) / len(word)) * len(word) )
-        //    ^ start     ^ word_idx             ^ hash
-        // = O( len(word) * len(s) )
-        for (int start = 0; start < word_len && start <= (int)s.size() - block_len; start++) {
-            unordered_multiset<string> visited;
-            size_t queue_size = 0;
-            size_t word_idx = 0;
-            unordered_set<string> ok_word;
-
-            while (start + (word_idx + 1) * word_len <= s.size()) {
-                int word_pos = start + word_idx * word_len;
-                string word = s.substr(word_pos, word_len);
-                if (word_set.count(word) == 0) {
-                    // not matched, clear queue & set
-                    queue_size = 0;
-                    visited.clear();
-                    ok_word.clear();
-                } else {
-                    // new matched word
-                    queue_size++;
-                    visited.insert(word);
-
-                    // visited counter modified, adjust ok_word set
-                    if (visited.count(word) == word_set.count(word)) {
-                        ok_word.insert(word);
-                    } else {
-                        ok_word.erase(word);
-                    }
-
-                    // queue too long, drop front
-                    if (queue_size > words.size()) {
-                        assert(queue_size == words.size() + 1);
-                        int index = word_pos - (queue_size - 1) * word_len;
-                        string queue_front = s.substr(index, word_len);
-
-                        // decrease found word count
-                        visited.erase(visited.equal_range(queue_front).first);
-
-                        // visited counter modified, adjust ok_word set
-                        if (visited.count(queue_front) == word_set.count(queue_front)) {
-                            ok_word.insert(queue_front);
-                        } else {
-                            ok_word.erase(queue_front);
-                        }
-
-                        queue_size--;
-                        assert(queue_size == words.size());
-                    }
-
-                    // anwser found
-                    if (queue_size == words.size() && ok_word.size() == uniq_word_count) {
-                        int index = word_pos - (queue_size - 1) * word_len;
-                        ans.push_back(index);
-                    }
-                }
-
-                word_idx++;
             }
         }
 
