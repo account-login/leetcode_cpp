ae022937cfbea06e6abfe94d8dee8cf9cad03e45
root <account-login@users.noreply.github.com>
2016-09-25 00:19:05 +0800

    [contain_dup_iii] add a multiset based solution.

--- src/contain_dup_iii.cpp
+++ src/contain_dup_iii.cpp
@@ -4,6 +4,7 @@
 #include <numeric>
 #include <cstdlib>
 #include <vector>
+#include <set>
 
 #ifdef RUN_TEST
 #   define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
@@ -12,6 +13,12 @@
 #   define NDEBUG   // remove assert() call
 #endif
 #include <cassert>
+
+// select implementation
+#define ALGO sort
+#define __SUFFIX(a, b) a##b
+#define _SUFFIX(a, b) __SUFFIX(a, b)
+#define containsNearbyAlmostDuplicate _SUFFIX(containsNearbyAlmostDuplicate_, ALGO)
 
 
 // https://leetcode.com/problems/contains-duplicate-iii/
@@ -22,7 +29,7 @@ using namespace std;
 
 class Solution {
 public:
-    bool containsNearbyAlmostDuplicate(const vector<int> &nums, int dist_lim, int val_lim) {
+    bool containsNearbyAlmostDuplicate_sort(const vector<int> &nums, int dist_lim, int val_lim) {
         assert(dist_lim >= 0);
         if (val_lim < 0) {
             return false;
@@ -50,6 +57,52 @@ public:
                 if (abs(indexes[i] - indexes[start]) <= dist_lim) {
                     return true;
                 }
+            }
+        }
+
+        return false;
+    }
+
+    bool containsNearbyAlmostDuplicate_tree(const vector<int> &nums, int dist_lim, int val_lim) {
+        assert(dist_lim >= 0);
+        if (val_lim < 0) {
+            return false;
+        }
+        if (dist_lim <= 0) {
+            return false;
+        }
+
+        multiset<int> tree;
+        int end = 0;
+        while (end < nums.size()) {
+            end++;
+            int mid = nums[end - 1];
+
+            auto it = tree.insert(mid);
+            if (it != tree.begin()) {
+                it--;
+                int left = *it;
+                it++;
+                if ((long long)mid - left <= val_lim) {
+                    return true;
+                }
+            }
+
+            it++;
+            if (it != tree.end()) {
+                int right = *it;
+                if ((long long)right - mid <= val_lim) {
+                    return true;
+                }
+            }
+
+            // tree is full, remove oldest one
+            if (tree.size() == dist_lim + 1) {
+                int begin = end - (dist_lim + 1);
+                assert(begin >= 0);
+                auto it = tree.find(nums[begin]);
+                assert(it != tree.end());
+                tree.erase(it);
             }
         }
 
@@ -84,6 +137,8 @@ TEST_CASE("220. Contains Duplicate III") {
 
     CHECK(s.containsNearbyAlmostDuplicate({ 1, 3, 5, 7 }, 3, 1) == false);
     CHECK(s.containsNearbyAlmostDuplicate({ 0, 3, 0, 5, 9 }, 2, 2) == true);
+    CHECK(s.containsNearbyAlmostDuplicate({ 0, 3, 10, 6, 0, 9 }, 3, 2) == true);
+    CHECK(s.containsNearbyAlmostDuplicate({ 0, 3, 7, 5, 9 }, 2, 2) == true);
     CHECK(s.containsNearbyAlmostDuplicate({ 0, 3, 0, 5, 9 }, 1, 2) == false);
     CHECK(s.containsNearbyAlmostDuplicate({}, 1, 1) == false);
     CHECK(s.containsNearbyAlmostDuplicate({ 0 }, 2, 1) == false);
