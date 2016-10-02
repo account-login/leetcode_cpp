23e07d130e841782c9bca97a52f8d2da529b04c8
root <account-login@users.noreply.github.com>
2016-10-02 15:33:43 +0800

    [merge_k] more effect heap operation: decrease heap top instead of pop/push.

--- src/merge_k.cpp
+++ src/merge_k.cpp
@@ -1,4 +1,6 @@
 #include <queue>
+#include <algorithm>
+#include <functional>
 #include <iostream>
 
 #ifdef RUN_TEST
@@ -15,6 +17,59 @@
 using namespace std;
 
 
+static inline int heap_left(int i) {
+    return (i * 2) + 1;
+}
+
+static inline int heap_right(int i) {
+    return (i * 2) + 2;
+}
+
+static inline int heap_parent(int i) {
+    return (i + 1) / 2 - 1;
+}
+
+
+template<typename ElemType, typename IterType, typename Comp>
+void heap_decrease_top(
+    IterType begin, IterType end, const ElemType &top,
+    Comp less_op = less<const ElemType &>())
+{
+    int len = end - begin;
+    assert(len > 0);
+
+    begin[0] = top;
+    for (int i = 0; ;) {
+        // no left
+        if (heap_left(i) >= len) {
+            return;
+        }
+
+        int swap_idx = i;
+        // left
+        if (less_op(top, begin[heap_left(i)])) {
+            swap_idx = heap_left(i);
+        }
+        // right
+        if (heap_right(i) < len) {
+            if (less_op(top, begin[heap_right(i)])
+                && less_op(begin[heap_left(i)], begin[heap_right(i)]))
+            {
+                swap_idx = heap_right(i);
+            }
+        }
+
+        // swap
+        if (swap_idx != i) {
+            swap(begin[i], begin[swap_idx]);
+            i = swap_idx;
+        } else {
+            return;
+        }
+    }
+}
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -25,7 +80,7 @@ using namespace std;
  */
 class Solution {
 public:
-    ListNode* mergeKLists(const vector<ListNode *> &lists) {
+    ListNode* mergeKLists_stl(const vector<ListNode *> &lists) {
         vector<ListNode *> lists_nn;    ///< copy of lists without nullptr
         copy_if(lists.begin(), lists.end(), back_inserter(lists_nn), [](const ListNode *node) {
             return node != nullptr;
@@ -37,6 +92,7 @@ public:
 
         ListNode head(0);
         ListNode *tail = &head;
+
         priority_queue<ListNode *, vector<ListNode *>, decltype(cmp)> q(cmp, move(lists_nn));
         while (!q.empty()) {
             tail->next = q.top();
@@ -45,6 +101,39 @@ public:
             q.pop();
             if (tail->next) {
                 q.push(tail->next);
+            }
+        }
+
+        assert(tail->next == nullptr);
+        auto ret = head.next;
+        head.next = nullptr;
+        return ret;
+    }
+
+    ListNode* mergeKLists(const vector<ListNode *> &lists) {
+        vector<ListNode *> heap;    ///< copy of lists without nullptr
+        copy_if(lists.begin(), lists.end(), back_inserter(heap), [](const ListNode *node) {
+            return node != nullptr;
+        });
+
+        auto cmp = [](const ListNode *n1, const ListNode *n2) {
+            return n1->val > n2->val;
+        };
+
+        ListNode head(0);
+        ListNode *tail = &head;
+
+        make_heap(heap.begin(), heap.end(), cmp);
+        while (!heap.empty()) {
+            tail->next = heap.front();
+            tail = tail->next;
+
+            if (tail->next) {
+                assert(tail->next->val >= heap[0]->val);
+                heap_decrease_top(heap.begin(), heap.end(), tail->next, cmp);
+            } else {
+                pop_heap(heap.begin(), heap.end(), cmp);
+                heap.pop_back();
             }
         }
 
@@ -79,6 +168,12 @@ TEST_CASE("23. Merge k Sorted Lists") {
     lists = { ListNode::from_vec({ 1, 4 }), ListNode::from_vec({ 2, 8 }), ListNode::from_vec({ -5, 10 }) };
     sorted = s.mergeKLists(lists);
     ans = { -5, 1, 2, 4, 8, 10 };
+    CHECK(sorted->to_vector() == ans);
+    delete sorted;
+
+    lists = { ListNode::from_vec({ 1, 4 }), ListNode::from_vec({ 4, 4 }), ListNode::from_vec({ -5, 4 }) };
+    sorted = s.mergeKLists(lists);
+    ans = { -5, 1, 4, 4, 4, 4 };
     CHECK(sorted->to_vector() == ans);
     delete sorted;
 }
